73dda8c131b960f6970ffd49d44d0f15f6f60f00
jihanyang <jihanyang13@gmail.com>
2021-06-13 16:13:24 +0800

    add training scripts

---
+++ tools/scripts/slurm_test_single.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
+
+PARTITION=$1
+GPUS=1
+GPUS_PER_NODE=1
+PY_ARGS=${@:2}
+JOB_NAME=eval
+SRUN_ARGS=${SRUN_ARGS:-""}
+
+srun -p ${PARTITION} \
+    --job-name=${JOB_NAME} \
+    --gres=gpu:${GPUS_PER_NODE} \
+    --ntasks=${GPUS} \
+    --ntasks-per-node=${GPUS_PER_NODE} \
+    --kill-on-bad-exit=1 \
+    -x SH-IDC1-10-5-36-214 \
+    ${SRUN_ARGS} \
+    python -u test.py ${PY_ARGS}
