5da32d248ea5114d4c7e06d29716fb48b2339fbf
Arthur K Zhang <arthurz@utexas.edu>
2023-04-03 17:25:27 -0500

    Added nuscenes to coda da

--- tools/scripts/ls6_slurn_train.bash
+++ tools/scripts/ls6_slurn_train.bash
@@ -18,14 +18,17 @@ export CUDA_VISIBLE_DEVICES=0,1,2
 # singularity exec --nv st3d.sif bash scripts/dist_train.sh 3 --cfg_file cfgs/nuscenes_models/pvrcnn/pvrcnn_oracle.yaml --batch_size 3
 
 #For nuscenes
-export PORT1=29500
-export CONFIG_FILE1=cfgs/nuscenes_models/pvrcnn/pvrcnn_oracle.yaml
-export NUSCENES_EXTRA_TAG=pvrcnn_oracle
+# export PORT1=29500
+# export CONFIG_FILE1=cfgs/nuscenes_models/pvrcnn/pvrcnn_oracle.yaml
+# export EXTRA_TAG1=pvrcnn_oracle
 
+export PORT1=29501
+export CONFIG_FILE1=cfgs/da-nuscenes-coda_models/pv_rcnn/pvrcnn_old_anchor.yaml
+export EXTRA_TAG1=pvrcnn_pretrain_coda_small 
 
 # Uncomment to launch nuscenes
 module load launcher_gpu
 export LAUNCHER_WORKDIR=/work/09156/arthurz/research/ST3D/tools
-export LAUNCHER_JOB_FILE=scripts/launcher_train_nuscenes
+export LAUNCHER_JOB_FILE=scripts/launcher_train_damodels
 
 ${LAUNCHER_DIR}/paramrun
