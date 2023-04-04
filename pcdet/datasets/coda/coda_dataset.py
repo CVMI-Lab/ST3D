46512c50c235a6cce185f77bf90352722f6a50a3
Arthur K Zhang <arthurz@utexas.edu>
2023-04-04 13:58:31 -0500

    Added coda class balanced sampling

--- pcdet/datasets/coda/coda_dataset.py
+++ pcdet/datasets/coda/coda_dataset.py
@@ -31,6 +31,9 @@ class CODataset(DatasetTemplate):
         self.coda_infos = []
         self.include_coda_data(self.mode)
 
+        if self.training and self.dataset_cfg.get('BALANCED_RESAMPLING', False):
+            self.coda_infos = self.balanced_infos_resampling(self.coda_infos)
+
     def include_coda_data(self, mode):
         if self.logger is not None:
             self.logger.info('Loading CODa dataset')
@@ -48,6 +51,44 @@ class CODataset(DatasetTemplate):
 
         if self.logger is not None:
             self.logger.info('Total samples for CODa dataset: %d' % (len(self.coda_infos)))
+    
+    def balanced_infos_resampling(self, infos):
+        """
+        Class-balanced sampling of CODa dataset from https://arxiv.org/abs/1908.09492
+        """
+        if self.class_names is None:
+            return infos
+
+        cls_infos = {name: [] for name in self.class_names}
+
+        for info in infos:
+            for name in set(info['annos']['name']):
+                if name in self.class_names:
+                    cls_infos[name].append(info)
+
+        duplicated_samples = sum([len(v) for _, v in cls_infos.items()])
+        cls_dist = {k: len(v) / duplicated_samples for k, v in cls_infos.items()}
+
+        sampled_infos = []
+
+        frac = 1.0 / len(self.class_names)
+        ratios = [frac / v for v in cls_dist.values()]
+
+        for cur_cls_infos, ratio in zip(list(cls_infos.values()), ratios):
+            sampled_infos += np.random.choice(
+                cur_cls_infos, int(len(cur_cls_infos) * ratio)
+            ).tolist()
+        self.logger.info('Total samples after balanced resampling: %s' % (len(sampled_infos)))
+
+        cls_infos_new = {name: [] for name in self.class_names}
+        for info in sampled_infos:
+            for name in set(info['annos']['name']):
+                if name in self.class_names:
+                    cls_infos_new[name].append(info)
+
+        cls_dist_new = {k: len(v) / len(sampled_infos) for k, v in cls_infos_new.items()}
+
+        return sampled_infos
 
     def set_split(self, split):
         super().__init__(
--- tools/scripts/ls6_slurn_train.bash
+++ tools/scripts/ls6_slurn_train.bash
@@ -22,11 +22,15 @@ export CUDA_VISIBLE_DEVICES=0,1,2
 # export CONFIG_FILE1=cfgs/nuscenes_models/pvrcnn/pvrcnn_oracle.yaml
 # export EXTRA_TAG1=pvrcnn_oracle
 
-export PORT=29501
-export CONFIG_FILE1=cfgs/da-nuscenes-coda_models/pv_rcnn/pvrcnn_old_anchor.yaml
-export EXTRA_TAG1=pvrcnn_pretrain_coda_small 
+# export PORT=29501
+# export CONFIG_FILE1=cfgs/da-nuscenes-coda_models/pv_rcnn/pvrcnn_old_anchor.yaml
+# export EXTRA_TAG1=pvrcnn_pretrain_coda_small 
 
-# Uncomment to launch nuscenes
+export PORT=29502
+export CONFIG_FILE1=cfgs/coda_models/pvrcnn_oracle.yaml
+export EXTRA_TAG1=pvrcnn_oracle_coda_small_resamp 
+
+# Uncomment to launch model training
 module load launcher_gpu
 export LAUNCHER_WORKDIR=/scratch/09156/arthurz/research/ST3D/tools
 export LAUNCHER_JOB_FILE=scripts/launcher_train_damodels
