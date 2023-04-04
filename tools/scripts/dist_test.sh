763dd9806e30f58d19bffb83004890e60b4364ad
artzha <arthurz@cs.utexas.edu>
2023-04-04 14:38:42 -0500

    Updated batch size and eval

--- tools/scripts/dist_test.sh
+++ tools/scripts/dist_test.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 set -x
-NGPUS=$1
+
+GPUS=$1
+NNODES=${NNODES:-1}
+NODE_RANK=${NODE_RANK:-0}
+PORT=${PORT:-29500}
+MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 PY_ARGS=${@:2}
 
-python -m torch.distributed.launch --nproc_per_node=${NGPUS} test.py --launcher pytorch ${PY_ARGS}
+
+python -m torch.distributed.launch --nproc_per_node=${GPUS} test.py --launcher pytorch ${PY_ARGS}
 
