a12186c15c0bf02c48e5bbe0544b80ab301a35e7
Arthur K Zhang <arthurz@cs.utexas.edu>
2023-04-06 12:06:02 -0500

    Updated dist test scripts

--- tools/scripts/dist_test.sh
+++ tools/scripts/dist_test.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
-set -x
-
 GPUS=$1
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
-PORT=${PORT:-29500}
+# PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 PY_ARGS=${@:2}
 
+while true
+do
+    PORT=$(( ((RANDOM<<15)|RANDOM) % 49152 + 10000 ))
+    status="$(nc -z 127.0.0.1 $PORT < /dev/null &>/dev/null; echo $?)"
+    if [ "${status}" != "0" ]; then
+        break;
+    fi
+done
+echo $PORT
 
-python -m torch.distributed.launch --nproc_per_node=${GPUS} test.py --launcher pytorch ${PY_ARGS}
-
+# PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
+python -m torch.distributed.launch \
+    --nnodes=$NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr=$MASTER_ADDR \
+    --nproc_per_node=$GPUS \
+    --master_port=$PORT \
+    test.py \
+    --launcher pytorch ${PY_ARGS}
