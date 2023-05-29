b52d9a10b5883382992711a904c75c6112b64dd5
Arthur K Zhang <arthurz@utexas.edu>
2023-05-29 17:30:11 -0500

    Decreased learning rate for da models

--- pcdet/models/roi_heads/target_assigner/proposal_target_layer.py
+++ pcdet/models/roi_heads/target_assigner/proposal_target_layer.py
@@ -218,6 +218,8 @@ class ProposalTargetLayer(nn.Module):
             if roi_mask.sum() > 0 and gt_mask.sum() > 0:
                 cur_roi = rois[roi_mask]
                 cur_gt = gt_boxes[gt_mask]
+                assert not torch.any(torch.isnan(rois))
+                assert not torch.any(torch.isnan(gt_boxes))
                 original_gt_assignment = gt_mask.nonzero().view(-1)
 
                 iou3d = iou3d_nms_utils.boxes_iou3d_gpu(cur_roi, cur_gt)  # (M, N)
@@ -225,7 +227,8 @@ class ProposalTargetLayer(nn.Module):
                 cur_max_overlaps, cur_gt_assignment = torch.max(iou3d, dim=1)
                 max_overlaps[roi_mask] = cur_max_overlaps
                 gt_assignment[roi_mask] = original_gt_assignment[cur_gt_assignment]
-
+        assert not torch.any(torch.isnan(max_overlaps))
+        assert not torch.any(torch.isnan(gt_assignment))
         # nan_mask = torch.isnan(max_overlaps)
         # max_overlaps[nan_mask] = 0
         # nan_mask = torch.isnan(gt_assignment)
