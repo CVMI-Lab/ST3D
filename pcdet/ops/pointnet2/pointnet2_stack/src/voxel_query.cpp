39b0cb83854ff56b14da4b1da46d5964994d3b87
Arthur K Zhang <arthurz@utexas.edu>
2023-07-14 13:33:09 -0500

    Updated cuda ops for st3d with debugging lines if needed

--- pcdet/ops/pointnet2/pointnet2_stack/src/voxel_query.cpp
+++ pcdet/ops/pointnet2/pointnet2_stack/src/voxel_query.cpp
@@ -1,16 +1,12 @@
-/*
-Stacked-batch-data version of ball query, modified from the original implementation of official PointNet++ codes.
-Written by Shaoshuai Shi
-All Rights Reserved 2019-2020.
-*/
-
-
 #include <torch/serialize/tensor.h>
 #include <vector>
 #include <THC/THC.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <cuda.h>
 #include <cuda_runtime_api.h>
-#include "ball_query_gpu.h"
+#include "voxel_query_gpu.h"
 
 extern THCState *state;
 
@@ -28,20 +24,21 @@ extern THCState *state;
 } while (0)
 #define CHECK_INPUT(x) CHECK_CUDA(x);CHECK_CONTIGUOUS(x)
 
-int ball_query_wrapper_stack(int B, int M, float radius, int nsample,
-    at::Tensor new_xyz_tensor, at::Tensor new_xyz_batch_cnt_tensor,
-    at::Tensor xyz_tensor, at::Tensor xyz_batch_cnt_tensor, at::Tensor idx_tensor) {
+
+int voxel_query_wrapper_stack(int M, int R1, int R2, int R3, int nsample, float radius, 
+    int z_range, int y_range, int x_range, at::Tensor new_xyz_tensor, at::Tensor xyz_tensor, 
+    at::Tensor new_coords_tensor, at::Tensor point_indices_tensor, at::Tensor idx_tensor) {
+    CHECK_INPUT(new_coords_tensor);
+    CHECK_INPUT(point_indices_tensor);
     CHECK_INPUT(new_xyz_tensor);
     CHECK_INPUT(xyz_tensor);
-    CHECK_INPUT(new_xyz_batch_cnt_tensor);
-    CHECK_INPUT(xyz_batch_cnt_tensor);
-
+    
     const float *new_xyz = new_xyz_tensor.data<float>();
     const float *xyz = xyz_tensor.data<float>();
-    const int *new_xyz_batch_cnt = new_xyz_batch_cnt_tensor.data<int>();
-    const int *xyz_batch_cnt = xyz_batch_cnt_tensor.data<int>();
+    const int *new_coords = new_coords_tensor.data<int>();
+    const int *point_indices = point_indices_tensor.data<int>();
     int *idx = idx_tensor.data<int>();
 
-    ball_query_kernel_launcher_stack(B, M, radius, nsample, new_xyz, new_xyz_batch_cnt, xyz, xyz_batch_cnt, idx);
+    voxel_query_kernel_launcher_stack(M, R1, R2, R3, nsample, radius, z_range, y_range, x_range, new_xyz, xyz, new_coords, point_indices, idx);
     return 1;
 }
