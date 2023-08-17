a1e9a7e5a187f79028394b8eb4b210458b8c201e
artzha <arthurz@cs.utexas.edu>
2023-08-17 12:53:40 -0500

    Added nus to jrdb training config

--- tools/scripts/ls6_slurn_jrdbtrain.sh
+++ tools/scripts/ls6_slurn_jrdbtrain.sh
@@ -31,20 +31,20 @@ export WANDB_API_KEY=dfd81f8955f7587d12b13da5256e56f80a89c014
 # export CONFIG_FILE1=cfgs/da-waymo-jrdb_models/centerhead_full/pvrcnn_pedonly.yaml
 # export EXTRA_TAG1=waymocodacfg
 
-# # NUS ped only
-# export PORT=29500
-# export CONFIG_FILE1=cfgs/da-nus-jrdb_models/centerhead_full/pvrcnn_pedonly.yaml
-# export EXTRA_TAG1=nuscodacfg
+# NUS ped only
+export PORT=29500
+export CONFIG_FILE1=cfgs/da-nus-jrdb_models/centerhead_full/pvrcnn_pedonly.yaml
+export EXTRA_TAG1=nuscodacfg
 
 # # JRDB scratch ped only no points filter
 # export PORT=29500
 # export CONFIG_FILE1=cfgs/jrdb_models/centerhead/pvrcnn_pedonly.yaml
 # export EXTRA_TAG1=jrdbcodacfgnofilter
 
-# JRDB scratch ped only 25m range (DOES LR SEARCH BY DEFAULT)
-export PORT=29500
-export CONFIG_FILE1=cfgs/jrdb_models/centerhead/pvrcnn_pedonly.yaml
-export EXTRA_TAG1=jrdbcodacfg25mrange
+# # JRDB scratch ped only 25m range (DOES LR SEARCH BY DEFAULT)
+# export PORT=29500
+# export CONFIG_FILE1=cfgs/jrdb_models/centerhead/pvrcnn_pedonly.yaml
+# export EXTRA_TAG1=jrdbcodacfg25mrange
 
 # # JRDB scratch ped only full range (DOES LR SEARCH BY DEFAULT)
 # export PORT=29500
